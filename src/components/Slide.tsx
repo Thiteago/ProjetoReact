03d85f4e9707828da8f761a60e5eea5c370b9ddc
Thiago <thiago_galaxy@hotmail.com>
2021-07-26 11:47:34 -0300

    Finish slide

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -5,50 +5,53 @@ import { SlideData } from './SlideData';
 
 
 const Slide = ({slides} : {slides : any}) => {
-const [current, setCurrent] = useState(0)
-const length = slides.lenght;
+
+    var [current, setCurrent] = useState(0)
+    const length = Object.keys(slides).length;
 
 
-const nextSlide = () =>{
-    setCurrent(current === length - 1 ? 0 : current + 1);
-    if(current === length){
-        setCurrent(0);
-    }
-};
+    const nextSlide = () =>{
+        if(current == length - 1){
+                setCurrent(0);
+            }else if(current < length - 1){
+                setCurrent(current + 1);
+            }
+    };
 
-useEffect(() => {
-    const interv = setInterval(nextSlide, 3000)
-    return () => {
-        clearInterval(interv);
-    }
-})
+    const img = document.getElementById("teste")
+    useEffect(() => {
+        const interv = setInterval(nextSlide, 3000)
+        return () => {
+            clearInterval(interv);
+        }
+    }, [img])
 
     return(
         <>
-        <section className="main-slide">
-            <div className="wrapper-slide">
-                <div className="legenda">
-                    <h1 id="titulo">Titulo</h1>
-                    <p id="descricao">Texto</p>
-                    <div className="clouds">
-                        <img src={nuvem} alt="nuvem esquerda"/>
-                        <img src={nuvem} alt="nuvem direita"/>
-                    </div>
-                </div>
-                <img className="wrapper-slide -fundo" src={nuvem} alt=""/>
-                {SlideData.map((slide, index) => {
-                    return(
-                        <div className={index === current ? 'slide active' : 'slide'} key={index}>
-                            {index === current && (<img className="wrapper-slide -mascara" id="img" src={slide.image} alt="produto imagem"/>)}
+            <section className="main-slide">
+                <div className="wrapper-slide">
+                    <div className="legenda">
+                        <h1 id="titulo">Titulo</h1>
+                        <p id="descricao">Texto</p>
+                        <div className="clouds">
+                            <img src={nuvem} alt="nuvem esquerda"/>
+                            <img src={nuvem} alt="nuvem direita"/>
                         </div>
-                    )
-                })}
-            </div>
+                    </div>
+                    <img className="wrapper-slide -fundo" src={nuvem} alt=""/>
+                    {SlideData.map((slide, index) => {
+                        return(
+                            <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                                {index === current && (<img className="wrapper-slide -mascara" id="teste" src={slide.image} alt="produto imagem"/>)}
+                            </div>
+                        )
+                    })}
+                </div>
 
-            <div className="nuvens">
-                <div className="Cloud"></div>
-            </div>
-        </section>
+                <div className="nuvens">
+                    <div className="Cloud"></div>
+                </div>
+            </section>
         </>
     );
 }
