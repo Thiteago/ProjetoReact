190aa8845ddd6104d2bed47a09f26fc56b2e5769
Thiteago <thiagodavidpd@gmail.com>
2022-08-12 10:56:31 -0300

    create component question

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -3,6 +3,10 @@ import "./Question.scss"
 export function Question(){
     return(
         <div className="wrapper-question">
+            <div className="title-question">
+                <h1>Voce come banana ?</h1>
+            </div>
+            
             
         </div>
     )
