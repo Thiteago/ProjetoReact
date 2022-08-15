5fe830539e43ca38faf1c8d0ddd099091d44a0d3
Thiteago <thiagodavidpd@gmail.com>
2022-08-15 11:31:01 -0300

    create animation on mande sua ideia page

--- src/pages/MandeSuaIdeia.tsx
+++ src/pages/MandeSuaIdeia.tsx
@@ -1,17 +1,34 @@
+import {useRef, useEffect} from 'react';
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 import "../styles/mandesuaideia.scss"
 import mande from "../assets/img/mande_sua_ideia.png";
 
 
+
 export function MandeSuaIdeia(){
+    const teste = useRef<HTMLDivElement>(null);
+
+    useEffect(() =>{
+        setInterval(() =>{
+            if(teste.current != null){
+                teste.current.style.display = 'none';
+            }
+        }, 7000);
+    }, [])
+
+    
     return(
         <>
             <Header></Header>
-            <div className="container-content">
+            <div ref={teste} className="container-content">
                 <div className="container-logo">
                     <img className="logo" src={mande} alt="" />
                 </div>
+            </div>
+
+            <div className="container-form">
+                
             </div>
 
             <Footer></Footer>
