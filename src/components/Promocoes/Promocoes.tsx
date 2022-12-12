885e15609eef81bf0a5d02b05ef36855132f6276
Thiago <thiagodavidpd@gmail.com>
2022-12-12 19:51:52 -0300

    removed owl-carousel dependency

--- src/components/Promocoes/Promocoes.tsx
+++ src/components/Promocoes/Promocoes.tsx
@@ -1,6 +1,4 @@
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 import {Card} from '../Cards/Card';
 
 
@@ -10,15 +8,7 @@ export function Promocoes(){
         <>
             <div className="main-comments">
                 <div className="slider"> 
-                    <OwlCarousel
-                    loop
-                    >
-                        <Card></Card>
-                        <Card></Card>
-                        <Card></Card>
-                        <Card></Card>
-                        <Card></Card>
-                    </OwlCarousel>
+
                 </div>
             </div>
         </>
